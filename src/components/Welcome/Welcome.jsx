08c652735fc0f2768ea37b90c69a01eb550fe300
Vinayak Garudi <vinayak.garudi@algonauts.in>
2023-02-25 12:29:21 +0530

    images added

--- src/components/Welcome/Welcome.jsx
+++ src/components/Welcome/Welcome.jsx
@@ -1,7 +1,12 @@
 import React, { useState } from 'react'
+import { useDispatch } from 'react-redux'
+import { actions } from '../../store/store'
 import './Welcome.css'
+import SpideyUpDown from '../../images/spidey-upside-down.webp'
+import Avengers from '../../images/avengers-team.webp'
 
 const Welcome = () => {
+    const dispatch = useDispatch()
     const [arrNum, setArrNum] = useState(0)
     const welcomeArr = [
         {
@@ -20,6 +25,19 @@ const Welcome = () => {
     return (
         <>
             <div className="welcome-container">
+                {
+                    arrNum == 0 ?
+                        <div className="img-spidey">
+                            <img src={SpideyUpDown} className="marvel-img" alt="" />
+                        </div> : ""
+                }
+                {
+                    arrNum == 1 ?
+                        <div className="img-avengers">
+                            <img style={{"opacity":"0.6"}} src={Avengers} className="marvel-img" alt="" />
+                        </div> : ""
+                }
+
                 <div className="welcome-content">
                     <div className="welcome-title">
                         {welcomeArr[arrNum].title}
@@ -27,10 +45,13 @@ const Welcome = () => {
                     <div className="welcome-desc">
                         {welcomeArr[arrNum].desc}
                     </div>
-
                     <div className="slide-container">
-                        <button className='slide-btn'>Previous</button>
-                        <button className='slide-btn'>Next</button>
+                        <button disabled={arrNum <= 0} onClick={() => setArrNum(arrNum - 1)} className='slide-btn'>Previous</button>
+                        {
+                            arrNum >= welcomeArr.length - 1 ?
+                                <button disabled={!(arrNum >= welcomeArr.length - 1)} onClick={() => dispatch(actions.toggleWelcome(false))} className='slide-btn'>Get Started</button> :
+                                <button disabled={arrNum >= welcomeArr.length - 1} onClick={() => setArrNum(arrNum + 1)} className='slide-btn'>Next</button>
+                        }
                     </div>
                 </div>
             </div>
